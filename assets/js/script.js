90d31a96dfa27a43e461904b542b8dcbe194085f
Your Name <you@example.com>
2021-03-24 09:38:01 -0400

    able to move task from one coloumn to another

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var taskIdCounter= 0;
 var formEl= document.querySelector("#task-form");
 var tasksToDoEl= document.querySelector("#tasks-to-do");
 var pageContentEl= document.querySelector("#page-content");
+var tasksInProgressEl= document.querySelector("#tasks-in-progress");
+var tasksCompletedEl= document.querySelector("#tasks-completed");
 
 // task form handler
 var taskFormHandler= function (event) {
@@ -154,6 +156,27 @@ var deleteTask= function(taskId) {
     taskSelected.remove();
 };
 
+var taskStatusChangeHandler= function(event) {
+    console.log(event.target);
+    console.log(event.target.getAttribute("data-task-id"));
+    console.log(event.target.value);
+    var taskId= event.target.getAttribute("data-task-id");
+    var statusValue= event.target.value.toLowerCase();
+    var taskSelected= document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    }
+    else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 formEl.addEventListener("submit", taskFormHandler);
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
